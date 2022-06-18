02c090b5e5c0c27c68899a13af728231c1885494
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-18 15:05:54 +0300

    filling the data of the answers migration

--- survey-BE/database/migrations/2022_06_18_113614_create_answers_table.php
+++ survey-BE/database/migrations/2022_06_18_113614_create_answers_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
+            $table->integer('question_id');
+            $table->longText('value');
             $table->timestamps();
         });
     }
