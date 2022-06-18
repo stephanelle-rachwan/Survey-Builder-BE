fc3b867d396af9728b406de447374e7e99c4202d
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-18 15:04:37 +0300

    filling the data of the choices migration

--- survey-BE/database/migrations/2022_06_18_113535_create_choices_table.php
+++ survey-BE/database/migrations/2022_06_18_113535_create_choices_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('choices', function (Blueprint $table) {
             $table->id();
+            $table->integer('question_id');
+            $table->longText('value');
             $table->timestamps();
         });
     }
