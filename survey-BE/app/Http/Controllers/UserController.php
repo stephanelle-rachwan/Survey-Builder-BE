770e898f371277996ceb980c766f637a204772a6
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-18 16:02:37 +0300

    creating the admin and user controllers

---
+++ survey-BE/app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Answer;
+use App\Models\Choice;
+use App\Models\Question;
+use App\Models\Survey;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    //
+}
--- survey-BE/routes/api.php
+++ survey-BE/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
 Route::group(['middleware' => 'api'], function($router) {
     Route::post('/register', [JWTController::class, 'register']);
