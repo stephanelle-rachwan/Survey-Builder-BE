209cbe8332d640ecd100b575a85b62fe10eba10d
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-18 14:58:30 +0300

    filling the data of the surveys migration

--- survey-BE/database/migrations/2022_06_18_112049_create_surveys_table.php
+++ survey-BE/database/migrations/2022_06_18_112049_create_surveys_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('surveys', function (Blueprint $table) {
             $table->id();
+            $table->string('survey_name');
             $table->timestamps();
         });
     }
