fae3167a44365739046db1de77aa46d4e839eedf
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-18 17:36:59 +0300

    creating the get surveys api

--- survey-BE/app/Http/Controllers/UserController.php
+++ survey-BE/app/Http/Controllers/UserController.php
@@ -12,17 +12,17 @@ use App\Models\User;
 class UserController extends Controller
 {
     //
-    // Route::get('/get-survey', [UserController::class, "getSurveys"]); 
+    // Route::get('/get-survey', [UserController::class, "getSurveys"]); done + tested
     // Route::get('/get-questions', [UserController::class, "getQuestions"]); 
     // Route::get('/get-choices', [UserController::class, "getChoices"]); 
     // Route::post('/add-answers', [UserController::class, "addAnswers"]); 
 
     public function getSurveys(Request $request)
     {
-        $items = Item::all();
+        $surveys = Survey::all();
         return response()->json([
             "status" => "success",
-            "items" => $items
+            "surveys" => $surveys
         ], 200);
     }
 }
