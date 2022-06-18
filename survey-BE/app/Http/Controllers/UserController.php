e6346de664d54b0343f4fd9d84c9886bbde72e50
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-18 18:36:59 +0300

    creating the get questions api

--- survey-BE/app/Http/Controllers/UserController.php
+++ survey-BE/app/Http/Controllers/UserController.php
@@ -25,4 +25,26 @@ class UserController extends Controller
             "surveys" => $surveys
         ], 200);
     }
+
+    public function getQuestions(Request $request)
+    {
+        // get questions by survey id
+        // http://127.0.0.1:8000/api/v1/user/get-questions?survey_id=1 hone we're getting it by survey_id is 1
+        // $survey_id=$request->survey_id;
+        // $questions = Question::where('survey_id', $survey_id)->get();
+
+        // get questions by survey name
+        $survey_name =$request->survey_name;
+        $survey_row = Survey::where('survey_name', $survey_name)->get();
+        $survey_id = $survey_row[0]['id'];
+        $questions = Question::where('survey_id', $survey_id)->get();
+
+        return response()->json([
+            "status" => "success",
+            "questions" => $questions
+        ], 200);
+    }
+
+    
+
 }
