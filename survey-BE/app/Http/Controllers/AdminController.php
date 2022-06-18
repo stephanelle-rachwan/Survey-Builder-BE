053aa578ac1e329e1d2809faa1ff6225fdc9f5cd
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-18 16:18:08 +0300

    creating the add survey api

--- survey-BE/app/Http/Controllers/AdminController.php
+++ survey-BE/app/Http/Controllers/AdminController.php
@@ -12,4 +12,20 @@ use App\Models\User;
 class AdminController extends Controller
 {
     //
+    // Route::post('/add-survey', [AdminController::class, "addSurveys"]); 
+    // Route::post('/add-questions', [AdminController::class, "addQuestions"]); 
+    // Route::post('/add-choices', [AdminController::class, "addChoices"]); 
+
+    public function addSurveys(Request $request)
+    {
+        $survey = new Survey;
+        $survey->survey_name = $request->survey_name;
+        $survey->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
+
+    
 }
--- survey-BE/routes/api.php
+++ survey-BE/routes/api.php
@@ -6,23 +6,27 @@ use App\Http\Controllers\JWTController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 
-Route::group(['middleware' => 'api'], function($router) {
-    Route::post('/register', [JWTController::class, 'register']);
-    Route::post('/login', [JWTController::class, 'login']);
-    Route::post('/logout', [JWTController::class, 'logout']);
-    Route::post('/refresh', [JWTController::class, 'refresh']);
-    Route::post('/profile', [JWTController::class, 'profile']);
-}); 
 
-Route::group(['prefix' => 'admin'], function(){
-    Route::post('/add-survey', [AdminController::class, "addSurveys"]); 
-    Route::post('/add-questions', [AdminController::class, "addQuestions"]); 
-    Route::post('/add-choices', [AdminController::class, "addChoices"]); 
-});
+Route::group(['prefix' => 'v1'], function(){
+    Route::group(['middleware' => 'api'], function($router) {
+        Route::post('/register', [JWTController::class, 'register']);
+        Route::post('/login', [JWTController::class, 'login']);
+        Route::post('/logout', [JWTController::class, 'logout']);
+        Route::post('/refresh', [JWTController::class, 'refresh']);
+        Route::post('/profile', [JWTController::class, 'profile']);
+    }); 
 
-Route::group(['prefix' => 'user'], function(){
-    Route::get('/get-survey', [UserController::class, "getSurveys"]); 
-    Route::get('/get-questions', [UserController::class, "getQuestions"]); 
-    Route::get('/get-choices', [UserController::class, "getChoices"]); 
-    Route::post('/add-answers', [UserController::class, "addAnswers"]); 
+    Route::group(['prefix' => 'admin'], function(){
+        Route::post('/add-survey', [AdminController::class, "addSurveys"]); 
+        Route::post('/add-questions', [AdminController::class, "addQuestions"]); 
+        Route::post('/add-choices', [AdminController::class, "addChoices"]); 
+    });
+
+    Route::group(['prefix' => 'user'], function(){
+        Route::get('/get-survey', [UserController::class, "getSurveys"]); 
+        Route::get('/get-questions', [UserController::class, "getQuestions"]); 
+        Route::get('/get-choices', [UserController::class, "getChoices"]); 
+        Route::post('/add-answers', [UserController::class, "addAnswers"]); 
+    });
+    
 });
